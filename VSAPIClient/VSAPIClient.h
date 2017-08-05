de143f8b3b6aa651f154ad5ed208334025b20a59
Vikas Shah <vik.k.shah@gmail.com>
2017-08-04 21:48:10 -0500

    Initial commit.

---
+++ VSAPIClient/VSAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  VSAPIClient.h
+//  VSAPIClient
+//
+//  Created by Vikas Shah on 8/4/17.
+//  Copyright © 2017 Vikas Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VSAPIClient.
+FOUNDATION_EXPORT double VSAPIClientVersionNumber;
+
+//! Project version string for VSAPIClient.
+FOUNDATION_EXPORT const unsigned char VSAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VSAPIClient/PublicHeader.h>
+
+
